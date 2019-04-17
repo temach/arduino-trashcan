3bd22fceb2bad3b38e8e92fbb671fadbc933d6a8
artem <tematibr@gmail.com>
2019-04-17 18:31:56 +0300

    updated flash reading sketch

--- flash_mem_to_uart/flash_mem_to_uart.ino
+++ flash_mem_to_uart/flash_mem_to_uart.ino
@@ -1,30 +1,40 @@
 /*
- This sketch shows how to request flash memory larger than defaut 4K, and read/write one specific word.
+ * This sketch demonstrates the use of flash memory
+ * 
+ * Ameba's flash memory can be edit in a unit of sector which has size of 
+ * 4K bytes.
+ *
+ * To read data from flash memory, we can just read it.
+ *
+ * To write data to flash memory, each bit on flash memory can only change
+ * from '1' to '0' and it cannot change from '0' to '1'. To make sure the
+ * data are correctly written we do erase flash memory sector before write
+ * data on it.
+ * 
+ * This sketch store boot count in flash. Each time device boot up, it read
+ * boot count from flash, add one, and write back to flash.
  */
 
 #include <FlashMemory.h>
 
 void setup() {
-  unsigned int value;
-  /* request flash size 0x4000 from 0xFC000 */
-  FlashMemory.begin(0xFC000, 0x4000);
-
-  /* read one word (32-bit) from 0xFC000 plus offset 0x3F00 */
-  value = FlashMemory.readWord(0x3F00);
-
-  printf("value is 0x%08X\r\n", value);
-
-  if (value == 0xFFFFFFFF) {
-    value = 0;
-  } else {
-    value++;
+  // Open serial communications and wait for port to open:
+  Serial.begin(115200);
+  while (!Serial) {
+    continue; // wait for serial port to connect. Needed for native USB port only
   }
-
-  /* write one word (32-bit) to 0xFC000 plus offset 0x3F00 */
-  FlashMemory.writeWord(0x3F00, value);
+  int k = 3.5;
+  // default start: 0xFF000
+  // sector size: 0x1000
+  FlashMemory.begin(0x00010000, 0x1000);
+  Serial.println("Hello ARTEM");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("start:");
+  for (unsigned int offset = 0; offset < 0x1000; offset += 4) {
+    Serial.print(FlashMemory.readWord(offset), HEX);
+  }
+   Serial.println("\n\n\n\n");
+  delay(2000);
 }
